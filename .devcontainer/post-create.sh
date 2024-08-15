129270b7590e3848b04a9eb08451672b33016bf6
s-tanaka <s-tanaka@iret.co.jp>
2024-08-15 12:41:36 +0000

    fix: git config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# git config --global --add safe.directory /workspaces
-# git config --global core.quotepath false
-
 sudo chown -R dev:dev /workspaces/node_modules/
 yarn install --frozen-lockfile

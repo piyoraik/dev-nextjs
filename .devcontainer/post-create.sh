657efe674670f82ed08ada93e04daff2c09b0b9f
s-tanaka <k1j8b6z5i7j9g2f5@cloudpack.slack.com>
2024-08-15 15:56:22 +0900

    fix: git config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git config --global --add safe.directory /workspaces
-git config --global core.quotepath false
+# git config --global --add safe.directory /workspaces
+# git config --global core.quotepath false
 
 sudo chown -R dev:dev /workspaces/node_modules/
 yarn install --frozen-lockfile
